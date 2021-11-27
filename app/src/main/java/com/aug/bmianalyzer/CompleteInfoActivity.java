a951ce79232994f7bdc54773f8ba4581b2213a2c
yasser147 <yass147er@gmail.com>
2021-11-27 12:02:02 +0200

    CompleteInfoActivity Just Xml

---
+++ app/src/main/java/com/aug/bmianalyzer/CompleteInfoActivity.java
@@ -0,0 +1,14 @@
+package com.aug.bmianalyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_info);
+    }
+}
