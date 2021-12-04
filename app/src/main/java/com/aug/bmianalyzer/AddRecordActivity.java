194d6acf8cfa1a48c72d955de1321ee9083d97c1
yasser147 <yass147er@gmail.com>
2021-12-04 22:12:00 +0200

    add record Just Xml

---
+++ app/src/main/java/com/aug/bmianalyzer/AddRecordActivity.java
@@ -0,0 +1,13 @@
+package com.aug.bmianalyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+    }
+}
